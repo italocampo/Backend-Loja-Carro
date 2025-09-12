d356ffc862c6ca2424b5a2a6cf68db45335caf7f
italocampo <italosouzacampos99@gmail.com>
2025-09-12 13:53:53 -0300

    correcao na porta server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import pino from 'pino';
 const logger = pino({ level: 'info' });
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  logger.info(`API v1 rodando em http://localhost:${PORT}/api/v1`);
+// A MUDANÇA ESTÁ AQUI: Adicionamos '0.0.0.0'
+// Isso permite que o servidor aceite conexões de fora do contêiner.
+app.listen(Number(PORT), '0.0.0.0', () => {
+  // A mensagem de log não precisa mudar, é apenas um texto informativo.
+  logger.info(`API v1 rodando em http://localhost:${PORT}/api/v1`);
 });
