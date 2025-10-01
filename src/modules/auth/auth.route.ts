1704197b81d8a881f8383ab13dd6f94d049b7385
italocampo <italosouzacampos99@gmail.com>
2025-09-30 21:01:53 -0300

    rota

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -9,10 +9,7 @@ const authRouter = Router();
 authRouter.post('/login', validateRequest(loginSchema), authController.login);
 authRouter.post('/logout', authController.logout);
 
-// ROTA QUE FALTAVA
 authRouter.post('/refresh', authController.refresh);
-
-// ROTA QUE ESTAVA DANDO 404
 authRouter.get('/me', isAuthenticated, authController.getMe);
 
 export { authRouter };
